0f8c6c7dfefe31d3376309022a375a5a034c6dc8
Dmitry Petukhov <dp@bsst.dev>
2023-08-28 12:19:03 +0500

    Fix test invocation

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -4,7 +4,7 @@ set -ex
 
 ./test_integer_conversion.py
 ./test_varnames.py
-./test_elements_script_tests.py ./script_tests_tapscript_opcodes.json
+./test_elements_script_tests.py ./script_tests_tapscript_opcodes.json tapscript
 if [ ! -e script_tests.json ]; then
     wget https://raw.githubusercontent.com/ElementsProject/elements/master/src/test/data/script_tests.json
 fi
